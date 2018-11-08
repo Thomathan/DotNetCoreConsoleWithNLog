d080fd5b0c01183cb8d5812164370d996c15c8bc
Tommy Wu <tommy.wu@tuangru.com>
2018-11-07 17:02:21 -0800

    first commit

---
+++ DotNetCoreConsoleWithNLog/ApplicationLifetimeHostedService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetCoreConsoleWithNLog {
+	public class ApplicationLifetimeHostedService : IHostedService {
+		private readonly IApplicationLifetime appLifetime;
+		private readonly ILogger<ApplicationLifetimeHostedService> logger;
+		private readonly IHostingEnvironment environment;
+		private readonly IConfiguration configuration;
+
+		public ApplicationLifetimeHostedService(
+			IConfiguration configuration,
+			IHostingEnvironment environment,
+			ILogger<ApplicationLifetimeHostedService> logger,
+			IApplicationLifetime appLifetime) {
+			this.configuration = configuration;
+			this.logger = logger;
+			this.appLifetime = appLifetime;
+			this.environment = environment;
+		}
+
+		public Task StartAsync(CancellationToken cancellationToken) {
+			this.logger.LogInformation("StartAsync method called.");
+
+			this.appLifetime.ApplicationStarted.Register(OnStarted);
+			this.appLifetime.ApplicationStopping.Register(OnStopping);
+			this.appLifetime.ApplicationStopped.Register(OnStopped);
+
+			return Task.CompletedTask;
+		}
+
+		private void OnStarted() {
+			this.logger.LogInformation("OnStarted method called.");
+
+			// Post-startup code goes here
+		}
+
+		private void OnStopping() {
+			this.logger.LogInformation("OnStopping method called.");
+
+			// On-stopping code goes here
+		}
+
+		private void OnStopped() {
+			this.logger.LogInformation("OnStopped method called.");
+
+			// Post-stopped code goes here
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken) {
+			this.logger.LogInformation("StopAsync method called.");
+
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ DotNetCoreConsoleWithNLog/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace DotNetCoreConsoleWithNLog {
+	internal class Program {
+		public static async Task Main(string[] args) {
+			IHost host = new HostBuilder()
+				 .ConfigureHostConfiguration(configHost => {
+					 configHost.SetBasePath(Directory.GetCurrentDirectory());
+					 configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+				 })
+				 .ConfigureAppConfiguration((hostContext, configApp) => {
+					 configApp.SetBasePath(Directory.GetCurrentDirectory());
+					 configApp.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+					 configApp.AddJsonFile($"appsettings.json", true);
+				 })
+				.ConfigureServices((hostContext, services) => {
+					ConfigureNLogServices(services);
+				})
+				.ConfigureLogging((hostContext, configLogging) => {
+					configLogging.AddNLog();
+				})
+				.UseConsoleLifetime()
+				.Build();
+
+			await host.RunAsync();
+		}
+
+		public static void ConfigureNLogServices(IServiceCollection services) {
+			var path = Path.GetFullPath("nlog.config");
+			var baseDir = GetBaseDir();
+
+			NLog.LogManager.LoadConfiguration(path);
+			NLog.LogManager.Configuration.Variables["basedir"] = baseDir;
+
+			services.AddLogging((x) => x.SetMinimumLevel(LogLevel.Trace));
+			var serviceProvider = services.BuildServiceProvider();
+
+			var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+			loggerFactory.AddNLog();
+		}
+
+		private static string GetBaseDir() {
+			var isWindowsRuntime = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+			var isLinuxRuntime = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+			var baseDir = string.Empty;
+
+			if (isLinuxRuntime) {
+				baseDir = "/opt/someDir";
+			} else if (isWindowsRuntime) {
+				baseDir = "C:\\ProgramData\\SomeFolder";
+			}
+
+			return baseDir;
+		}
+	}
+}
